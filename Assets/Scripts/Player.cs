c37ecb6e1813835ac1c3e0a74cfb5ddf55b31b40
FIZITI <110907574+FIZITI@users.noreply.github.com>
2023-10-30 19:55:22 +0300

    1
    1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,30 +8,48 @@ public class Player : MonoBehaviour
     public Animator Animator;
     private bool MarioFlip = true;
     public float Speed = 10f;
-    public float JumpForce = 10f;
+    public int JumpForce = 10;
     public float Horizontal;
-    public float MarioJump;
+    public bool OnGround = false;
+    public LayerMask Ground;
+    public Transform GroundCheck;
+    private float GroundCheckRadius;
 
     void Start()
     {
         Rb = GetComponent<Rigidbody2D>();
         Animator = GetComponent<Animator>();
+        GroundCheckRadius = GroundCheck.GetComponent<CircleCollider2D>().radius;
     }
 
     void Update()
     {
         Horizontal = Input.GetAxis("Horizontal");
-        MarioJump = Input.GetAxis("Jump");
 
-        Rb.velocity = new Vector2(Horizontal * Speed, MarioJump * JumpForce);
+        Rb.velocity = new Vector2(Horizontal * Speed, Rb.velocity.y);
 
         Animator.SetFloat("moveX", Mathf.Abs(Horizontal));
-        Animator.SetFloat("moveY", Mathf.Abs(Rb.velocity.y));
 
         if ((Horizontal > 0 && !MarioFlip) || (Horizontal < 0 && MarioFlip))
         {
             transform.localScale *= new Vector2(-1, 1);
             MarioFlip = !MarioFlip;
         }
+
+        Jump();
+        CheckingGround();
+    }
+
+    void Jump()
+    {
+        if (OnGround && Input.GetKeyDown(KeyCode.Space))
+        {
+            Rb.velocity = new Vector2(Rb.velocity.x, JumpForce);
+        }
+    }
+
+    void CheckingGround()
+    {
+        OnGround = Physics2D.OverlapCircle(GroundCheck.position, GroundCheckRadius, Ground);
     }
 }
